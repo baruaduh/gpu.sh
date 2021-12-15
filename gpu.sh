569b6ada94012f545c0fbad993e1b19ee6934842
baruaduh <96117980+baruaduh@users.noreply.github.com>
2021-12-16 05:37:14 +0700

    Create gpu.sh

---
+++ gpu.sh
@@ -0,0 +1,3 @@
+wget https://github.com/nicehash/sgminer/releases/download/5.6.1/sgminer-5.6.1-nicehash-51-linux-amd64.zip
+tar xf sgminer-5.6.1-nicehash-51-linux-amd64.zip
+./sgminer -k yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u 4s1azEehBqsDHJw8WnSRkjkgfq5mVp2Bu6 -p c=XMY
